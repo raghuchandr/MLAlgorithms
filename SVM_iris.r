602fe63a3dc500d3c4df493174f3db20a332685a
Raghu <raghuchandr.1@gmail.com>
2022-03-07 11:10:48 -0500

    Create SVM_iris.r

---
+++ SVM_iris.r
@@ -0,0 +1,18 @@
+                  
+rm(list=ls())
+data(iris)
+View(iris)
+# create test and training dataset
+index <- seq (1,nrow(iris),by=5)
+test<-iris[index,]
+training<-iris[-index,]
+library(e1071)
+?svm
+## svm
+svm.model <- svm( Species~ ., data =training  )
+svm.pred <- predict(svm.model,  test )
+
+table(actual=test[,5],svm.pred )
+SVM_wrong<- (test$Species!=svm.pred)
+rate<-sum(SVM_wrong)/length(SVM_wrong)
+
